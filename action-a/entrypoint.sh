226393c2fc2b00cea49e1f6b4979882f00eb1be4
Alexander Nurzhanov <Alexander.Nurzhanov@md.de>
2020-12-09 09:59:40 +0100

    initial commit

---
+++ action-a/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -l
+
+sh -c "Willkommen auf GitHub!"
+sh -c "echo Hallo $INPUT_MY_NAME, du hast es geschafft deinen ersten GitHub Action erfolgreich auszuführen!"
+sh -c "echo Herzlichen Glückwunsch $INPUT_MY_NAME!"
